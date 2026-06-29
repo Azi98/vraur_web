9d57839949c3fe979e1093ef91c6dbd19a22e36a
Aznaur <bedraev98@gmail.com>
2026-06-29 22:06:31 +0200

    Add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { siteDescription, siteName, siteUrl } from "./seo";
 
@@ -44,6 +45,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18284882309"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18284882309');
+          `}
+        </Script>
+      </head>
       <body className={`${instrumentSans.variable} antialiased`}>
         {children}
       </body>
